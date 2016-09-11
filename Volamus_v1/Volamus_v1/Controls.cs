ace0270d40083fc20543a26aa56e25cfb720ca79
DrGengar <larshaider@yahoo.de>
2016-09-11 18:05:23 +0200

    mark_pfeil verändert (besser jetzt!)

--- Volamus_v1/Volamus_v1/Controls.cs
+++ Volamus_v1/Volamus_v1/Controls.cs
@@ -84,17 +84,6 @@ namespace Volamus_v1
             GameStateManager.Instance.SpriteBatch.DrawString(font, "Spin Left", new Vector2(20, 450), Color.White);
             GameStateManager.Instance.SpriteBatch.DrawString(font, "Spin Right", new Vector2(20, 500), Color.White);
 
-            //Power-Ups
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "POWER UPS:", new Vector2(30, 600), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Ball Bigger", new Vector2(30, 650), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Light Blue", new Vector2(400, 650), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Balls Smaller", new Vector2(30, 700), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Dark Blue", new Vector2(400, 700), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Enemy Velocity Down", new Vector2(30, 750), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Light Green", new Vector2(400, 750), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Own Velocity Up", new Vector2(30, 800), Color.White);
-            GameStateManager.Instance.SpriteBatch.DrawString(font, "Dark green", new Vector2(400, 800), Color.White);
-
             //Keyboard
             GameStateManager.Instance.GraphicsDevice.Viewport = leftView;
 
