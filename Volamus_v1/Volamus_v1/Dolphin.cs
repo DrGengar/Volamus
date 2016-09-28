7958dbdd59ea52e32524fe08b7a2aaf68db1d25e
iam <lena.spitz@gmx.net>
2016-09-28 19:48:35 +0200

    delfine and texturen geupdated

--- Volamus_v1/Volamus_v1/Dolphin.cs
+++ Volamus_v1/Volamus_v1/Dolphin.cs
@@ -18,7 +18,7 @@ namespace Volamus_v1
 
         public new void LoadContent()
         {
-            Texture = GameStateManager.Instance.Content.Load<Texture2D>("Textures/pinguinUV");
+            Texture = GameStateManager.Instance.Content.Load<Texture2D>("Textures/delfinUV");
             model = GameStateManager.Instance.Content.Load<Model>("Models/delfin");
             CreateBoundingBoxes();
 
